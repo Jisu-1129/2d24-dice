81431931f0d997c5c854152897cac0dd91a6fed0
Jisu-1129 <115052208+Jisu-1129@users.noreply.github.com>
2022-10-05 09:37:47 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let count = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
     basic.pause(1000)
@@ -10,4 +11,12 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(5)
     basic.pause(1000)
     basic.showNumber(6)
+})
+input.onButtonPressed(Button.B, function () {
+    count = 10
+    for (let index = 0; index < 10; index++) {
+        count += -1
+        basic.showNumber(count)
+        basic.pause(1000)
+    }
 })
