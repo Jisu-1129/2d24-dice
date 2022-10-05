3753685bdeaa350203de09cb40d3e3e7b778e2b8
Jisu-1129 <115052208+Jisu-1129@users.noreply.github.com>
2022-10-05 09:20:36 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     basic.showNumber(1)
+    basic.pause(1000)
+    basic.showNumber(2)
+    basic.pause(1000)
+    basic.showNumber(3)
+    basic.pause(1000)
+    basic.showNumber(4)
+    basic.pause(1000)
+    basic.showNumber(5)
+    basic.pause(1000)
+    basic.showNumber(6)
 })
